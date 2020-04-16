08e37b05ddb6a36b974f80d75bbb22e624bea66e
Karthik Suresh <kartie.suresh@gmail.com>
2020-04-15 21:24:20 -0400

    Update Asure_Insertion.py

--- Asure_Insertion.py
+++ Asure_Insertion.py
@@ -1,10 +1,10 @@
 import pyodbc
 import os
 from IPython.display import clear_output
-server = 'bdat-social.database.windows.net' 
-database = 'Tim_Data' 
-username = 'bdat' 
-password = 'Password@123' 
+server = 'YOUR SERVER NAME' 
+database = 'YOUR DB NAME' 
+username = 'USERNAME' 
+password = 'PASSWORD@123' 
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 from ipywidgets import IntProgress
@@ -17,7 +17,7 @@ import pandas as pd
 # In[11]:
 
 
-df = pd.read_excel('tim_data.xlsx')
+df = pd.read_excel('FILE.XLSX')
 df.head()
 
 
@@ -30,7 +30,7 @@ max_count = 300
 # In[65]:
 
 
-tquery = "TRUNCATE table [dbo].[tim_horton_db]"
+tquery = "TRUNCATE TABLE <TABLNAME>"
 cursor.execute(tquery)
 print("Database Truncated!")
 
@@ -42,7 +42,7 @@ f = IntProgress(min=0, max=max_count)
 display(f)
 for index, row in df.iterrows():
     row['Review'] = row['Review'].replace("'","")
-    query = "INSERT INTO [dbo].[tim_horton_db] VALUES ({},'{}',{},{},'{}','{}','{}','{}','{}',{},{},'{}','{}')".format(row['sno'],row['Location'],row['Latitude'],row['Longitude'],row['Postal_Code'],row['Current_Status'],row['Contact_No'],row['Open'],row['Close'],row['Number_of_Reviews'],row['Overall_Rating'],row['Review'],row['polarity'])
+    query = "INSERT INTO <TABLENAME> VALUES ({},'{}',{},{},'{}','{}','{}','{}','{}',{},{},'{}','{}')".format(row['sno'],row['Location'],row['Latitude'],row['Longitude'],row['Postal_Code'],row['Current_Status'],row['Contact_No'],row['Open'],row['Close'],row['Number_of_Reviews'],row['Overall_Rating'],row['Review'],row['polarity'])
     cursor.execute(query)
     cnxn.commit()
     f.value += 1 
